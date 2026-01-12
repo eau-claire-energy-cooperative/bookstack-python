efe44f2388a698a0a2681e07b92c55bab9718160
Rob Weber <rweber@ecec.com>
2026-01-12 09:16:08 -0600

    updated version information

--- src/bookstack/__version__.py
+++ src/bookstack/__version__.py
@@ -1,7 +1,7 @@
 __author__ = 'coffeepenbit'
 __description__ = "A Python wrapper for BookStack's API"
 __author_email__ = 'coffeepenbit@gmail.com'
-__maintainer__ = 'coffeepenbit'
-__title__ = 'bookstack'
-__url__ = "https://github.com/coffeepenbit/bookstack"
-__version__ = '0.2.0-post1'
+__maintainer__ = 'rweber'
+__title__ = 'bookstack-python'
+__url__ = "https://github.com/eau-claire-energy-cooperative/bookstack-python"
+__version__ = '0.2.1'
