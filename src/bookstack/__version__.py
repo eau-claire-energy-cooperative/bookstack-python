2cd03c31fce3bea3b07846f8227396cd5ec985ce
Rob Weber <rweber@ecec.com>
2026-04-09 15:45:05 -0500

    version bump

--- src/bookstack/__version__.py
+++ src/bookstack/__version__.py
@@ -4,4 +4,4 @@ __author_email__ = 'coffeepenbit@gmail.com'
 __maintainer__ = 'rweber'
 __title__ = 'bookstack-python'
 __url__ = "https://github.com/eau-claire-energy-cooperative/bookstack-python"
-__version__ = '0.2.1'
+__version__ = '0.2.2'
